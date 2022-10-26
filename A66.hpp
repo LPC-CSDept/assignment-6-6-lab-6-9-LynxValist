656c33b34ff42d43c2219ce2cc209b386ce1b332
Gerald Kyro Villanueva Tungol <Lynxvillicana@gmail.com>
2022-10-25 18:41:55 -0700

    test stuffs

--- A66.hpp
+++ A66.hpp
@@ -1,7 +1,35 @@
-//
-
-#include <iomanip>
-#include <iostream>
+#include    <iostream>
+#include    <iomanip>
 using namespace std;
+
+void getinput(int, int, int);
+void maxmin(int &, int &, int &, int &, int &);
+
+void maxmin(int &num1, int &num2, int &num3, int &max, int &min){
+    if(num1>num2){
+        if(num1>num3)
+            max = num1;
+        else{
+            max = num3;
+            min = num2;
+        }
+        if(num2>num3)
+            min = num3;
+        else
+            min = num2;
+    }
+    else{
+        if(num2> num3)
+            max = num2;
+        else{
+            max = num3;
+            min = num1;
+        }
+        if(num1>num3)
+            min = num3;
+        else
+            min = num1;
+    }
+}
 
 // complete the function maxmin( )
